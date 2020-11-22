8b9f0438727a66dea5022f573149d477eaff24df
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2020-11-21 22:14:23 -0600

    telegramServer: Add bodyParser

--- curso-de-backend-con-node-js/telegramServer/server.js
+++ curso-de-backend-con-node-js/telegramServer/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
+const router = express.Router();
 const app = express();
 
 const PORT = 8000;
 
-app.use('/', (req, res) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(router) // router ALWAYS goes to the end of "use" handles
+
+router.get('/', (req, res) => {
     res.send('Hola');
+});
+
+router.post('/', (req, res) => {
+  console.log(req.query)
+  console.log(req.body)
+  res.send('El post');
 });
 
 app.listen(PORT, () =>
