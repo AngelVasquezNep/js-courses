28680d14f4ac136f290c6939a21eb85217e823e5
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2020-12-11 17:32:29 -0600

    javascriptForReact: Template literals class

---
+++ javascriptForReact/template-literals/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {String[]} strings - Los strings que haya dentro del template literal
+ * @param {Array} values - Los valores dinámicos
+ *
+ */
+
+function stronger(strings, ...values) {
+  let newMessage = "";
+
+  for (index in strings) {
+    const strongedMessage =
+      (values[index] && `<strong>${values[index]}</strong>`) || "";
+    newMessage += `${strings[index]}${strongedMessage}`;
+  }
+
+  console.log(newMessage);
+
+  return newMessage;
+}
+
+const message = stronger`Hola ${"mundo"} de React`;
+
+console.log(message);
+
+// Hola <strong>mundo</strong> de React
