7fcce9ba143ceacaf6866e7a6f043064c64ba981
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2020-12-18 14:55:39 -0600

    platziPosts: All DB functions are return a Promise

--- curso-platzi-practico-de-node/store/db.js
+++ curso-platzi-practico-de-node/store/db.js
@@ -2,20 +2,20 @@ const db = {
     users: [{ id: 1, name: 'Angelito' }],
 };
 
-function list(table, query) {
+async function list(table, query) {
     return db[table];
 }
 
-function get(table, id) {
+async function get(table, id) {
     return list(table).find((item) => item.id === id);
 }
 
-function upsert(table, data) {
+async function upsert(table, data) {
     db[table].push(data);
     return data;
 }
 
-function remove(table, id) {
+async function remove(table, id) {
     return true;
 }
 
