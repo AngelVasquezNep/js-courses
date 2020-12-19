42e37bbf8a928660c9a8c796bb5deff7f57971be
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2020-12-19 13:17:16 -0600

    platziPosts: Add express.json at server to manage body requests

--- curso-platzi-practico-de-node/api/index.js
+++ curso-platzi-practico-de-node/api/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 
-const routes = require('./routes')
+const routes = require('./routes');
 const config = require('../config.js');
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
-routes(app)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+routes(app);
 
 app.listen(config.api.PORT, () => {
     console.log(`> Server listenning at ${config.api.API_BASE_URL}`);
