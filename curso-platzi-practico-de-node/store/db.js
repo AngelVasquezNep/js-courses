b448e61c8178311dfa96e7b3c880cfc159558919
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2020-12-19 13:16:49 -0600

    platziPosts: Update Store database methods

--- curso-platzi-practico-de-node/store/db.js
+++ curso-platzi-practico-de-node/store/db.js
@@ -1,5 +1,7 @@
+const uuidv4 = require('../lib/uuidv4');
+
 const db = {
-    users: [{ id: 1, name: 'Angelito' }],
+    users: [{ id: '1', name: 'Angelito', createdAt: new Date(), updatedAt: new Date() }],
 };
 
 async function list(table, query) {
@@ -7,21 +9,56 @@ async function list(table, query) {
 }
 
 async function get(table, id) {
-    return list(table).find((item) => item.id === id);
+    return list(table).then((list) => list.find((item) => item.id === id));
 }
 
-async function upsert(table, data) {
-    db[table].push(data);
-    return data;
+async function create(table, data) {
+    const createdAt = new Date();
+
+    const element = {
+        ...data,
+        createdAt,
+        updatedAt: createdAt,
+        id: uuidv4(),
+    };
+
+    db[table].push(element);
+
+    return element;
+}
+
+async function update(table, id, data) {
+    const elementIndex = db[table].findIndex((el) => el.id === id);
+
+    if (elementIndex !== -1) {
+        const element = db[table][elementIndex];
+
+        const newElement = {
+            ...element,
+            ...data,
+            id,
+            createdAt: element.createdAt,
+            updatedAt: new Date(),
+        };
+
+        db[table][elementIndex] = newElement;
+
+        return newElement;
+    }
+
+    throw new Error(`Element ${id} at [${table}] was Not found`);
 }
 
 async function remove(table, id) {
+    db[table] = db[table].filter((element) => element.id !== id);
+
     return true;
 }
 
 module.exports = {
     list,
     get,
-    upsert,
+    create,
+    update,
     remove,
 };
