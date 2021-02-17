33462f7119e359b1da22a5cf0e95d1e6770b0bc9
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-02-17 01:31:38 -0600

    platziPosts: Use mysql into store

--- curso-platzi-practico-de-node/store/db.js
+++ curso-platzi-practico-de-node/store/db.js
@@ -3,75 +3,45 @@ const mysql = require('./mysql');
 const utils = require('../utils');
 const uuidv4 = require('../lib/uuidv4');
 
-const db = {
-    auth: [],
-    users: [
-        {
-            id: '1',
-            name: 'Angelito',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        },
-    ],
-};
-
-async function list(table, query = {}) {
-    return mysql.list(table);
+async function list(table, query) {
+    return mysql.list(table, query);
 }
 
 async function get(table, id) {
-    const element = list(table).then((list) =>
-        list.find((item) => item.id === id),
-    );
+    return mysql.get(table, id).then((element) => {
+        if (!element) {
+            throw utils.errors.error(404, `Element ${id} was not found`);
+        }
 
-    if (!element) {
-        throw utils.errors.error(404, `Element ${id} was not found`);
-    }
-
-    return element;
+        return element;
+    });
 }
 
 async function create(table, data, { useCustomId = false } = {}) {
-    const createdAt = new Date().getTime();
-
-    const element = {
+    return mysql.create(table, {
         ...data,
-        createdAt,
-        updatedAt: createdAt,
         id: useCustomId ? data.id : uuidv4(),
-    };
-
-    db[table].push(element);
-
-    return element;
+    });
 }
 
 async function update(table, id, data) {
-    const elementIndex = db[table].findIndex((el) => el.id === id);
+    return mysql.update(table, id, data).then((element) => {
+        if (!element) {
+            throw utils.errors.error(404, `Element ${id} was not found`);
+        }
 
-    if (elementIndex !== -1) {
-        const element = db[table][elementIndex];
-
-        const newElement = {
-            ...element,
-            ...data,
-            id,
-            createdAt: element.createdAt,
-            updatedAt: new Date().getTime(),
-        };
-
-        db[table][elementIndex] = newElement;
-
-        return newElement;
-    }
-
-    throw utils.errors.error(404, `Element ${id} was not found`);
+        return element;
+    });
 }
 
 async function remove(table, id) {
-    db[table] = db[table].filter((element) => element.id !== id);
+    return mysql.remove(table, id).then((result) => {
+        if (result.affectedRows === 0) {
+            throw utils.errors.error(404, `Element ${id} was not found`);
+        }
 
-    return true;
+        return true;
+    });
 }
 
 module.exports = {
