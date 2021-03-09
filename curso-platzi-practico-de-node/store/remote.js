42270c91d9aceebc82f4d7e997b0d80ac7689c97
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-03-08 21:51:58 -0600

    platziPosts: Create missing remote store methods

--- curso-platzi-practico-de-node/store/remote.js
+++ curso-platzi-practico-de-node/store/remote.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const { errors } = require('../utils');
 
+const composeURL = (...params) => params.join('/');
+
 function req({ method, url, body, query }) {
     return axios({
         method,
@@ -15,7 +17,7 @@ function req({ method, url, body, query }) {
         .catch((error) => {
             const { data } = error.response;
 
-            console.error(`[REMOVE DB ERROR] ${JSON.stringify(data)}`);
+            console.error(`[REMOTE DB ERROR] ${JSON.stringify(data)}`);
 
             throw errors.error(400);
         });
@@ -25,16 +27,33 @@ function CreateRemoteDB(dbUrl) {
     function list(table, query, join) {
         return req({
             method: 'GET',
-            url: `${dbUrl}/${table}`,
-            query: {
-                ...query,
-                join,
-            },
+            url: composeURL(dbUrl, table),
+            query: { ...query, join },
         });
+    }
+
+    function create(table, body) {
+        return req({ method: 'POST', url: composeURL(dbUrl, table), body });
+    }
+
+    function get(table, id) {
+        return req({ method: 'GET', url: composeURL(dbUrl, table, id) });
+    }
+
+    function update(table, id, body) {
+        return req({ method: 'PUT', url: composeURL(dbUrl, table, id), body });
+    }
+
+    function remove(table, id) {
+        return req({ method: 'DELETE', url: composeURL(dbUrl, table, id) });
     }
 
     return {
         list,
+        create,
+        get,
+        update,
+        remove,
     };
 }
 
